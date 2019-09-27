fced12a595c3579a5151da0750a30c119e53c6f3
Cody Jackson <codyjackson@pop-os.localdomain>
2019-09-27 16:29:43 -0500

    Initial commit

---
+++ factorial_functs.h
@@ -0,0 +1,10 @@
+//
+// Created by codyjackson on 9/27/19.
+//
+
+#ifndef SWIG_FACTORIAL_FUNCTS_H
+#define SWIG_FACTORIAL_FUNCTS_H
+long long int factorial(long long int num);
+int get_modulus(int num1, int num2);
+
+#endif //SWIG_FACTORIAL_FUNCTS_H
